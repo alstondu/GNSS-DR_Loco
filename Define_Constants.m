ede575a06450b162879cf6499dd2582d35c9e6ae
alstondu <dudu2545874349@gmail.com>
2024-02-11 15:36:10 +0000

    Defined constants for use

---
+++ Define_Constants.m
@@ -0,0 +1,13 @@
+%Define_Constants
+%SCRIPT This defines a number of constants for use
+
+% Constants
+deg_to_rad = 0.01745329252; % Degrees to radians conversion factor
+rad_to_deg = 1/deg_to_rad; % Radians to degrees conversion factor
+c = 299792458; % Speed of light in m/s
+omega_ie = 7.292115E-5;  % Earth rotation rate in rad/s
+Omega_ie = Skew_symmetric([0,0,omega_ie]);
+R_0 = 6378137; %WGS84 Equatorial radius in meters
+e = 0.0818191908425; %WGS84 eccentricity
+
+% Ends
